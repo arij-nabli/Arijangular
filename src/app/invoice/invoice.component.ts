80c8431ec3cb2183dd6c84234d2831a3ffafa301
arij-nabli <arij.nabli@esprit.tn>
2023-10-08 22:43:22 +0100

    Atelier3

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -11,12 +11,21 @@ export class InvoiceComponent implements OnInit {
   id: number | null = null;
   active: boolean | null = null;
   invoice: Invoice | null = null;
+  invoiceList = [
+    { id: 1, montantFacture: 120, montantRemise: 10, dateFacture: '12/12/2021', active: true },
+    { id: 2, montantFacture: 1020, montantRemise: 90, dateFacture: '22/11/2020', active: true },
+    { id: 3, montantFacture: 260, montantRemise: 30, dateFacture: '18/10/2020', active: false },
+    { id: 4, montantFacture: 450, montantRemise: 40, dateFacture: '14/12/2020', active: true }
+  ];
   constructor(
     private route: ActivatedRoute,
     private router: Router 
   ) { }
 
- 
+ getInvoiceById(id: number): any {
+    // Recherchez la facture dans la liste par ID
+    return this.invoiceList.find(invoice => invoice.id === id);
+  }
 
   ngOnInit() {
     
@@ -25,15 +34,8 @@ export class InvoiceComponent implements OnInit {
       this.active = params['active'] === 'true'; 
 
       
-      if (this.active) {
-        
-        this.invoice = {
-          idFacture: this.id,
-          montantFacture: 50,
-          montantRemise: 50,
-          dateFacture: '01/01/2023',
-          active: true
-        };
+     if (this.active) {
+        this.invoice = this.getInvoiceById(this.id);
       } else {
         
         this.invoice = null;
